39e5ce6ad93a54dd0dfda3611851b951dd380fc3
skanwngud <skanwngud@gmail.com>
2021-02-14 17:44:05 +0900

    commit

---
+++ mask/crawling_2_repeat.py
@@ -0,0 +1,57 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+import urllib.request
+
+# select webbrowser (chrome)
+driver=webdriver.Chrome()
+
+# link to address
+driver.get('https://www.google.co.kr/imghp?hl=ko&tab=ri&ogbl')
+
+# fine specified elements
+elem=driver.find_element_by_name('q')
+
+# input keys & enter
+elem.send_keys('사람 얼굴')
+elem.send_keys(Keys.RETURN)
+
+# scroll web page
+SCROLL_PAUSE_TIME=1
+last_height=driver.execute_script('return document.body.scrollHeight')
+# 스크롤 높이를 java Script 로 찾아서 last_height 란 변수에 저장 시킴
+
+while True: # 무한 반복
+    driver.execute_script('window.scrollTo(0, document.body.scrollHeight);')
+    # 스크롤을 끝까지 내린다
+
+    time.sleep(SCROLL_PAUSE_TIME) # 스크롤이 끝나면 0.5초동안 기다림
+
+    new_height=driver.execute_script('return document.body.scrollHeight')
+    if new_height==last_height:
+        try:
+            driver.find_element_by_css_selector('.mye4qd').click()
+            # 결과 더보기 버튼 클릭
+        except:
+            break
+    last_height=new_height
+
+# select & click image in webbrowser 
+images=driver.find_elements_by_css_selector('.rg_i.Q4LuWd')
+count=1
+
+for image in images:
+    try:
+        image.click()
+        time.sleep(2)
+        imgUrl=driver.find_element_by_css_selector('.n3VNCb').get_attribute('src')
+        # opener=urllib.request.build_opener()
+        # opener.addheaders=[('User-Agent','Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1941.0 Safari/537.36')]
+        # urllib.request.install_opener(opener)
+        # urllib.request.urlretrieve(imgUrl, "test.jpg")
+        # Forbidden 이 뜨면 위의 코드를 추가한다
+        urllib.request.urlretrieve(imgUrl, 'no_mask'+str(count)+'.jpg')
+        count=count+1
+    except:
+        pass
+driver.close()
