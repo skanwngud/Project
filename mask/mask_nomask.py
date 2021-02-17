84f4118bcd0b5c8e85142270a0a5b41b37b6372b
skanwngud <skanwngud@gmail.com>
2021-02-17 09:45:17 +0900

    commit

--- mask/mask_nomask.py
+++ mask/mask_nomask.py
@@ -13,5 +13,24 @@ from keras.preprocessing.image import ImageDataGenerator
 
 from PIL import Image
 
-
 from sklearn.model_selection import train_test_split
+
+img_list=glob('c:/datasets/face_train/human/mask/*.jpg')
+img_list_2=glob('c:/datasets/face_train/human/nomask/*.jpg')
+
+data=list()
+label=list()
+for i in img_list:
+    img=cv2.imread(i)
+    img=cv2.resize(img, (256, 256))
+    img=np.array(img)/255.
+    data.append(img)
+    label.append(0)
+
+for i in img_list_2:
+    img=cv2.imread(i)
+    img=cv2.resize(img, (256, 256))
+    img=np.array(img)/255.
+    data.append(img)
+    label.append(1)
+
