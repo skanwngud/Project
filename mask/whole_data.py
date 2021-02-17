4a47be2315dd7e2c4d0ee1cf4d249fc7f20cfd87
skanwngud <skanwngud@gmail.com>
2021-02-17 20:49:55 +0900

    commit

--- mask/whole_data.py
+++ mask/whole_data.py
@@ -6,38 +6,33 @@ import cv2
 
 from keras.models import load_model
 
-imglist=glob.glob('c:/datasets/face_train/all/*.jpg')
-
+imglist=glob.glob('e:/datasets/face_train/all/*.jpg')
 
 img_list=list()
 for i in imglist:
-    try:
-        img=tensorflow.keras.preprocessing.image.load_img(
-            i,
-            target_size=(256, 256),
-            color_mode='rgb',
-            interpolation='lanczos'
-        )
-        img=np.array(img)/255.
-        img_list.append(img)
-    except:
-        pass
+    img=cv2.imread(i)
+    img=cv2.resize(img, (256, 256))
+    img=np.array(img)
+    img_list.append(img)
+
+img_list=np.array(img_list)
+# img_list=img_list.reshape(-1, 256, 256, 1)
 
 # print(img_list[0].shape) # (512, 512, 3)
 
-x_train=np.load('c:/datasets/face_train/x_train.npy')
-y_train=np.load('c:/datasets/face_train/y_train.npy')
-x_test=np.load('c:/datasets/face_train/x_test.npy')
-y_test=np.load('c:/datasets/face_train/y_test.npy')
-x_val=np.load('c:/datasets/face_train/x_val.npt.npy')
-y_val=np.load('c:/datasets/face_train/y_val.npy')
+x_train=np.load('c:/data/npy/pro_x_train.npy')
+y_train=np.load('c:/data/npy/pro_y_train.npy')
+x_test=np.load('c:/data/npy/pro_x_test.npy')
+y_test=np.load('c:/data/npy/pro_y_test.npy')
+x_val=np.load('c:/data/npy/pro_x_val.npy')
+y_val=np.load('c:/data/npy/pro_y_val.npy')
 
 model=load_model(
-    'c:/data/modelcheckpoint/project_0.6214_0.7452.hdf5'
+    'c:/data/modelcheckpoint/project.hdf5'
 )
 
-loss=model.evaluate(x_test, y_test)
-pred=model.predict(np.array(img_list))
+# loss=model.evaluate(x_test, y_test)
+pred=model.predict(img_list)
 pred=np.where(pred>0.5, 1, 0) # 1 은 사람, 0 은 파레이돌리아
 
 
@@ -45,7 +40,7 @@ print('전체 사진 : ', len(img_list))
 print('사람 : ', np.count_nonzero(pred))
 print('비율 : ', np.count_nonzero(pred)/len(img_list))
 
-print('loss : ', loss)
+# print('loss : ', loss)
 
 
 # results
