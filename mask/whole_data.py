31f3931d56356422488707343166fea15b836834
skanwngud <skanwngud@gmail.com>
2021-02-19 11:42:20 +0900

    commit

--- mask/whole_data.py
+++ mask/whole_data.py
@@ -1,57 +1,45 @@
-import numpy as np
-import glob
 import tensorflow
-
+import tensorflow as tf
+import numpy as np
 import cv2
 
+
+from glob import glob
+
+from keras.preprocessing.image import ImageDataGenerator
 from keras.models import load_model
 
-imglist=glob.glob('e:/datasets/face_train/all/*.jpg')
+model=load_model('c:/data/modelcheckpoint/best_project.hdf5')
 
-img_list=list()
-for i in imglist:
-    img=cv2.imread(i)
-    img=cv2.resize(img, (256, 256))
-    img=np.array(img)
-    img_list.append(img)
+img_list=glob('c:/dataset/mask/export/images/*.jpg')
 
-img_list=np.array(img_list)
-# img_list=img_list.reshape(-1, 256, 256, 1)
+test=list()
 
-# print(img_list[0].shape) # (512, 512, 3)
+datagen=ImageDataGenerator()
 
-x_train=np.load('c:/data/npy/pro_x_train.npy')
-y_train=np.load('c:/data/npy/pro_y_train.npy')
-x_test=np.load('c:/data/npy/pro_x_test.npy')
-y_test=np.load('c:/data/npy/pro_y_test.npy')
-x_val=np.load('c:/data/npy/pro_x_val.npy')
-y_val=np.load('c:/data/npy/pro_y_val.npy')
+for i in img_list:
+    try:
+        img=cv2.imread(i)
+        img=cv2.resize(img, (256, 256))
+        img=np.array(img)/255.
+        test.append(img)
+    except:
+        pass
 
-model=load_model(
-    'c:/data/modelcheckpoint/project.hdf5'
-)
+test=np.array(test)
+test=datagen.flow()
 
-# loss=model.evaluate(x_test, y_test)
-pred=model.predict(img_list)
-pred=np.where(pred>0.5, 1, 0) # 1 은 사람, 0 은 파레이돌리아
+pred=model.predict_generator(test)
+pred=np.argmax(test, axis=-1)
 
-
-print('전체 사진 : ', len(img_list))
-print('사람 : ', np.count_nonzero(pred))
-print('비율 : ', np.count_nonzero(pred)/len(img_list))
-
-# print('loss : ', loss)
-
+print(type(pred[0]))
+print('전체 : ', len(img_list)) # 1632
+print('마스크사람 : ', len(img_list)-np.count_nonzero(pred))
+print('마스크비율 : ', np.count_nonzero(pred)/len(img_list))
+print(pred[0])
 
 # results
-
-# ideal
-# about 0.7
-
-# 전체 사진 :  2534
-# 사람 :  1331
-# 비율 :  0.5252565114443567
-# loss :  [0.6427163481712341, 0.7045454382896423]
-
-# project_0.6214_0.7452
-# 
+# 전체 :  1009
+# 마스크사람 :  -2018
+# 마스크비율 :  3.0
+# [0.37070063 0.5508767  0.07842268]
