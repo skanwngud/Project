1920df00500f7fa12d001d6417620dac94a4e103
skanwngud <skanwngud@gmail.com>
2021-08-08 00:18:52 +0900

    commit

--- selfie/crawling.py
+++ selfie/crawling.py
@@ -1,6 +1,23 @@
-import torch
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+import urllib.request
 
-print(torch.cuda.is_available()) # true
+# 1. 웹브라우저와 드라이버를 선택 (크롬)
+# chromedriver.exe 파일이 crawling.py 파일과 같은 경로에 있어야함
+driver = webdriver.Chrome()
 
-from tensorflow.keras.models import Sequential, Model
-from tensorflow.keras.layers import Dense, Conv2D
+# 2. 크롬 브라우저를 이용해 해당 주소로 이동함
+driver.get('https://www.shutterstock.com/ko/search/one+people?image_type=photo')
+
+# 3. 웹페이지의 특정 요소를 찾음 (검색을 하기 위함)
+elem = driver.find_element_by_name('searchterm')
+
+# 4. 원하는 검색어를 입력
+elem.send_keys('one people')
+
+# 5. enter 키 입력
+elem.send_keys(Keys.RETURN)
+
+# 6. 사진의 각 요소를 선택 및 클릭
+driver.find_elements_by_css_selector('z_h_9d80b z_h_2f2f0')[0].click()
