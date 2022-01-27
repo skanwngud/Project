7b567fac2d2b71021936082f533a9d8f070860b7
hyeong1145 <hyeong1145@4-lab.com>
2022-01-27 16:10:47 +0900

    2022-01-27 (make html files)

--- money/app.py
+++ money/app.py
@@ -12,12 +12,17 @@ def home():
 
 @app.route("/register")
 def register():
-    return "register"
+    return render_template("register.html")
 
 
 @app.route("/main")
 def main_page():
-    return "main page"
+    return render_template("main.html")
+
+
+@app.route("/manager")
+def manager_page():
+    return render_template("manager.html")
 
 
 if __name__ == "__main__":
