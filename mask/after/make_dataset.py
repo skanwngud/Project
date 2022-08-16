6e18f1fb1e0604310dd707128f7db4a0ac8fb3bf
skanwngud <skanwngud@gmail.com>
2022-08-16 23:04:15 +0900

    2022-08-16

--- mask/after/make_dataset.py
+++ mask/after/make_dataset.py
@@ -1,5 +1,4 @@
 from glob import glob
-from PIL import Image, ImageEnhance, ImageChops
 
 import cv2
 import numpy as np
@@ -11,45 +10,19 @@ class MakeDataset:
 
     # Brightness
     def chg_brightness(self, image, enhance_value):
-        enhanceer = ImageEnhance.Brightness(image)
-        birghtness_image = enhanceer.enhance(enhance_value)
-
-        if self.save:
-            birghtness_image.save(self.save)
-
-        return birghtness_image
+        pass
 
     # Flip horizaontal
     def flip_hroizontal(self, image):
-        horizontal_flip_image = image.transpose(Image.FLIP_LEFT_RIGHT)
-
-        if self.save:
-            horizontal_flip_image.save(self.save)
-
-        return horizontal_flip_image
+        pass
 
     # FLip vertical
     def flip_vertical(self, image):
-        vertical_flip_image = image.transpose(Image.FLIP_TOP_BOTTOM)
-
-        if self.save:
-            vertical_flip_image.save(self.save)
-
-        return vertical_flip_image
+        pass
 
     # move to horizontal
     def move_to_horizontal(self, image, px=0):
-        width, height = image.size
-        shift = random.randint(0, int(width * px))
-
-        horizontal_shift_image = ImageChops.offset(image, shift, 0)
-        horizontal_shift_image.paste((0), (0, 0, shift, height))
-
-        if self.save:
-            horizontal_shift_image.save(self.save)
-
-        return horizontal_shift_image
-
+        pass
 
 if __name__ == "__main__":
     img = Image.open("../face.jpg")
