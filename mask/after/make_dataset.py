6364aadfa38a203f2df7b27d9b43103b81a9d635
skanwngud <skanwngud@gmail.com>
2022-08-16 23:37:32 +0900

    2022-08-16

--- mask/after/make_dataset.py
+++ mask/after/make_dataset.py
@@ -9,26 +9,58 @@ class MakeDataset:
         self.save = save
 
     # Brightness
-    def chg_brightness(self, image, enhance_value):
-        pass
+    def chg_brightness(self, image, type):
+        """
+        :param image: original image
+        :param type: "b" is brighter, "d" is darker
+        :return: changed image
+        """
 
-    # Flip horizaontal
-    def flip_hroizontal(self, image):
-        pass
+        assert type in ["b", "d"], "please choose 'b' or 'd'"
+        temp_array = np.full(image.shape, (128, 128, 128), dtype=np.uint8)
 
-    # FLip vertical
-    def flip_vertical(self, image):
-        pass
+        if type == "b":
+            chg_img = cv2.add(image, temp_array)
+        elif type == "d":
+            chg_img = cv2.subtract(image, temp_array)
+
+        if self.save:
+            cv2.imwrite(self.save, chg_img)
+
+        return chg_img
+
+    # Flip horizontal, vertical
+    def flip_hroizontal(self, image, type):
+        """
+        :param image: original image
+        :param type: "h" is horizontal, "v" is vertical
+        :return: changed image
+        """
+        assert type in ["h", "v"], "please input 'h' or 'v'"
+
+        if type == "h":
+            flip_img = cv2.flip(image, 1)
+        elif type == "v":
+            flip_img = cv2.flip(image, 0)
+
+        if self.save:
+            cv2.imwrite(self.save, flip_img)
+
+        return flip_img
 
     # move to horizontal
     def move_to_horizontal(self, image, px=0):
         pass
 
+    # Rotation image
+    def rotation_img(self, image, angle):
+        pass
+
+
 if __name__ == "__main__":
-    img = Image.open("../face.jpg")
-    img = img.convert("RGB")
+    img = cv2.imread('../face.jpg')
 
     md = MakeDataset("./test.jpg")
-    # md.chg_brightness(img, 1.8, )
-    # md.flip_hroizontal(img)
-    md.move_to_horizontal(img, 0.2)
+    # md.chg_brightness(img, "d")
+    md.flip_hroizontal(img, "v")
+    # md.move_to_horizontal(img, 0.2)
